86be1cb93f896f80cece544c563a56fc34d82f43
igleson <iglesonfreire1993@gmail.com>
2015-01-28 11:20:24 -0300

    multiple creators

--- js/posts.js
+++ js/posts.js
@@ -6,6 +6,21 @@ app.filter('slice', function() {
     };
 });
 
+app.filter('creators', function() {
+    return function(creators) {
+        var ret = creators[0];
+        if(creators.length == 1) {
+            return ret;
+        }
+
+        for (var i = 1; i < creators.length -1; i++){
+            ret += ', ' + creators[i];
+        }
+        ret += ' e ' + creators[creators.length-1];
+        return ret;
+    };
+});
+
 app.config(['$stateProvider', '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/posts/page/1');
@@ -22,47 +37,47 @@ function($stateProvider, $urlRouterProvider) {
 }]);
 
 posts = [
-    {title:'Lorem ipsum1', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum2', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum3', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum4', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum5', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum6', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum7', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum8', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum9', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum10', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum11', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum12', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum13', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum14', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum15', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum16', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum17', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum18', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum19', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum20', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum21', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum22', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum23', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum24', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum25', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum26', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum27', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum28', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum29', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum30', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum31', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum32', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum33', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum34', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum35', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum36', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum37', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum38', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum39', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum39', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'},
-    {title:'Lorem ipsum40', subtitle:'dolor sit amet, consectetur adipiscing elit', creator:'Igleson', date:'26-01-2014', htmlSrc:'teste.html'}
+    {title:'Lorem ipsum01', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum02', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum03', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum04', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum05', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum06', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum07', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum08', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum09', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum10', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum11', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum12', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum13', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum14', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum15', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum16', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum17', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum18', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum19', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum20', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum21', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum22', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum23', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum24', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum25', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum26', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum27', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum28', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum29', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum30', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum31', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum32', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum33', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum34', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum35', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum36', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum37', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum38', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum39', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum39', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'},
+    {title:'Lorem ipsum40', subtitle:'dolor sit amet, consectetur adipiscing elit', creators:['Igleson','Igleson','Igleson'], date:'26-01-2014', htmlSrc:'teste.html'}
 ];
 
 app.controller('postsController', function ($scope, $stateParams, $location) {
